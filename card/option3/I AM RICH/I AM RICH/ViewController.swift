db2d735bb0b1e9b601bb0da619e37810f36f02e3
Paa Kwesi Amamoo <67609834+senseiBlaq@users.noreply.github.com>
2025-04-26 09:35:10 +0100

    Add files via upload

---
+++ card/option3/I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by sensei_blaq on 05/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ card/option3/I AM RICH/challenge_/challenge_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge_
+//
+//  Created by sensei_blaq on 06/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
